56ec9b17b45ba82b04aa7b409fde78128e74ee2b
xiaoss <982637685@qq.com>
2025-03-16 11:20:20 +0800

    feat：项目配置

---
+++ lite/src/test/java/com/zt/eweb/modular/rbac/service/RbacRoleQueryServiceTest.java
@@ -0,0 +1,64 @@
+package com.zt.eweb.modular.rbac.service;
+
+import com.wujiuye.flow.FlowHelper;
+import com.wujiuye.flow.FlowType;
+import com.wujiuye.flow.Flower;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 模块名 :
+ * 文件名 :
+ * 创建时间 : 2025/3/16 10:42
+ * 实现功能 :
+ * <p>
+ * 作者 : xiaoss
+ * 版本 : V1.0.0-SNAPSHOT
+ *
+ * @see
+ * @since ----------------------------------------------------------------
+ * 修改记录
+ * 日 期     	版本     修改人  修改内容
+ * 2025/3/16      V1.0.0  xiaoss   创建
+ * ----------------------------------------------------------------
+ */
+@Slf4j
+@DisplayName("角色管理服务测试")
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class RbacRoleQueryServiceTest {
+    @Autowired
+    private RbacRoleQueryService    rbacRoleQueryService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("分页查询角色列表")
+    void testQueryRolePage() {
+
+        rbacRoleQueryService.queryRolePage(null);
+    }
+    private FlowHelper flowHelper = new FlowHelper(FlowType.Hour);
+
+    @Test
+    void countQPS(){
+        Flower flower = flowHelper.getFlow(FlowType.Hour);
+        System.out.println("总请求数:"+flower.total());
+        System.out.println("成功请求数:"+flower.totalSuccess());
+        System.out.println("异常请求数:"+flower.totalException());
+        System.out.println("平均请求耗时:"+flower.avgRt());
+        System.out.println("最大请求耗时:"+flower.maxRt());
+        System.out.println("最小请求耗时:"+flower.minRt());
+        System.out.println("平均请求成功数(每毫秒):"+flower.successAvg());
+        System.out.println("平均请求异常数(每毫秒):"+flower.exceptionAvg());
+        System.out.println();
+    }
+}
+
