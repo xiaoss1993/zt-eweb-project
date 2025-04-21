5152c4850f43178d0443814a08ebd0c2b235d443
sizt <982637685@qq.com>
2025-04-21 18:23:54 +0800

    2025年04月21日18:22:00

--- framework/zt-eweb-commons/src/main/java/com/zt/eweb/framework/common/domain/StatusEnum.java
+++ framework/zt-eweb-commons/src/main/java/com/zt/eweb/framework/common/domain/StatusEnum.java
@@ -1,22 +1,24 @@
-package com.zt.eweb.framework.common.base.domain;
+package com.zt.eweb.framework.common.domain;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * 用户状态枚举
  *
+ * @author haoxin
+ * @date 2021-02-02
  **/
 public enum StatusEnum implements ValueObject<StatusEnum> {
 
     /**
      * 有效
      */
-    ENABLE("0", "有效"),
+    ENABLE("0","有效"),
 
     /**
      * 禁用
      */
-    DISABLE("1", "禁用");
+    DISABLE("1","禁用");
 
 
     private String value;
@@ -42,12 +44,12 @@ public enum StatusEnum implements ValueObject<StatusEnum> {
      * @param value
      * @return
      */
-    public static String getLabelByValue(String value) {
-        if (StringUtils.isBlank(value)) {
+    public static String getLabelByValue(String value){
+        if(StringUtils.isBlank(value)) {
             return "";
         }
         for (StatusEnum s : StatusEnum.values()) {
-            if (value.equals(s.getValue())) {
+            if(value.equals(s.getValue())){
                 return s.getLabel();
             }
         }
@@ -60,12 +62,12 @@ public enum StatusEnum implements ValueObject<StatusEnum> {
      * @param value
      * @return
      */
-    public static StatusEnum getStatusEnum(String value) {
-        if (StringUtils.isBlank(value)) {
+    public static StatusEnum getStatusEnum(String value){
+        if(StringUtils.isBlank(value)) {
             return null;
         }
         for (StatusEnum s : StatusEnum.values()) {
-            if (value.equals(s.getValue())) {
+            if(value.equals(s.getValue())){
                 return s;
             }
         }
